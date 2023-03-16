faa7dfa276ef04b623bb409ce361c8121cd0e525
akashdnb <akashranjandnb@gmail.com>
2023-03-17 00:08:39 +0530

    Added app icon and splash screen animation

--- app/src/main/java/com/example/innorevaresourcemanager/SplashActivity.java
+++ app/src/main/java/com/example/innorevaresourcemanager/SplashActivity.java
@@ -8,20 +8,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.example.innorevaresourcemanager.databinding.ActivitySplashBinding;
 
 import java.util.Objects;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Objects.requireNonNull(getSupportActionBar()).hide();
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        try {
+            binding.webView.loadUrl("file:///android_asset/splash.html");
+        }catch (Exception e){
+            Toast.makeText(this, ""+e.toString(), Toast.LENGTH_SHORT).show();
+        }
 
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
